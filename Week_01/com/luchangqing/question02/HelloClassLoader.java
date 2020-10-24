0beb079484d8cd5223622599bcf125edaa514f7d
luchangqing <525601201@qq.com>
2020-10-24 10:20:16 +0800

    流关闭

--- Week_01/com/luchangqing/question02/HelloClassLoader.java
+++ Week_01/com/luchangqing/question02/HelloClassLoader.java
@@ -7,7 +7,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class HelloClassLoader extends ClassLoader {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
         Class<?> hello = new HelloClassLoader().findClass("Hello");
         Object instance = hello.newInstance();
         Method method = hello.getMethod("hello");
@@ -15,10 +15,10 @@ public class HelloClassLoader extends ClassLoader {
     }
 
     @Override
-    protected Class<?> findClass(String name) throws ClassNotFoundException {
+    protected Class<?> findClass(String name) {
 
         byte[] bytes = null;
-        try (InputStream resourceAsStream = this.getResourceAsStream("Hello.xlass")){
+        try (InputStream resourceAsStream = this.getResourceAsStream("Hello.xlass")) {
             bytes = toByteArray(resourceAsStream);
         } catch (IOException e) {
             e.printStackTrace();
@@ -26,19 +26,24 @@ public class HelloClassLoader extends ClassLoader {
         return defineClass(name, bytes, 0, bytes.length);
     }
 
-    public static byte[] toByteArray(InputStream input) throws IOException {
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int n = 0;
-        while (-1 != (n = input.read(buffer))) {
-            output.write(buffer, 0, n);
+    public static byte[] toByteArray(InputStream input) {
+        byte[] result = null;
+        try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
+            byte[] buffer = new byte[1024];
+            int n = 0;
+            while (-1 != (n = input.read(buffer))) {
+                output.write(buffer, 0, n);
+            }
+            byte[] bytes = output.toByteArray();
+            result = new byte[bytes.length];
+            int i = 0;
+            for (byte b : bytes) {
+                result[i++] = (byte) (255 - b);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        byte[] bytes = output.toByteArray();
-        byte[] result = new byte[bytes.length];
-        int i = 0;
-        for (byte b : bytes) {
-            result[i++] = (byte) (255 - b);
-        }
+
         return result;
     }
 }
